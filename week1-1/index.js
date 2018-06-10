bedd8f3e38ab283dc9d0b3adba79f13e74919892
chandu <chandrasr.palle@gmail.com>
2018-06-10 15:21:11 +0200

    week1-1 updated

--- week1-1/index.js
+++ week1-1/index.js
@@ -4,13 +4,17 @@ const util = require('util');
 console.log("hello excercise 2");
 
 const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile)
 
 const readFileOne = readFile("text1.txt",'utf8');
 const readFileTwo = readFile('text2.txt','utf8');
 
 Promise.all([readFileOne, readFileTwo]).then((data)=>{
-console.log("Both files are read");
-console.log(data.join(' & '));
+//console.log("Both files are read");
+//console.log(data.join(' & '));
+writeFile("result.txt", data.join()).then(()=>{
+    console.log("File wrote");
+});
 }).catch(()=>{
 console.log("error");
 });

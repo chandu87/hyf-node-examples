4101067e5b290fde6e2c34206bf8eed512db5871
chandu <chandrasr.palle@gmail.com>
2018-06-13 17:01:19 +0200

    test

---
+++ Introtonode/echo.js
@@ -0,0 +1,6 @@
+function echo(string, number){
+for(let i =0; i < number ; i++){
+    console.log(string);
+} 
+}
+echo("hi",10);
---
+++ Introtonode/excercise3.js
@@ -0,0 +1,15 @@
+var catMe = require('cat-me');
+
+var scores = [ 90, 98, 89, 100, 100, 86, 94];
+console.log(Math.round(average(scores)));
+
+function average(scores){
+    let total= 0;
+    scores.forEach(element => {
+    total += element;
+});
+
+return total/scores.length;
+}
+console.log(catMe());   
+console.log("i"); 

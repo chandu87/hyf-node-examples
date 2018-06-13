b8f74bebd95d819b100a5b64d8adee63b6801e91
chandu <chandrasr.palle@gmail.com>
2018-06-14 00:57:00 +0200

    basic server started

--- Introtoexpress/FirstExpressApp/app.js
+++ Introtoexpress/FirstExpressApp/app.js
@@ -1,1 +1,10 @@
-console.log("Our express App comes here");
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+    res.send("Hi there");
+});
+
+app.listen(3000, function(){
+    console.log("Server started at Port : 3000");
+})

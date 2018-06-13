5fc50afea069d28cfc51f8bd54b53c77f02e3ab0
chandu <chandrasr.palle@gmail.com>
2018-06-14 01:03:34 +0200

    more request added

--- Introtoexpress/FirstExpressApp/app.js
+++ Introtoexpress/FirstExpressApp/app.js
@@ -4,7 +4,12 @@ var app = express();
 app.get("/", function(req, res){
     res.send("Hi there");
 });
-
+app.get("/bye", function(req, res){
+    res.send("Bye Bye");
+});
+app.get("/cat", function(req, res){
+    res.send("Meow!!!");
+})
 app.listen(3000, function(){
     console.log("Server started at Port : 3000");
 })

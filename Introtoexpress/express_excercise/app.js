80834cd99e498e7236d7c75e6d0c8ef0df189a43
chandu <chandrasr.palle@gmail.com>
2018-06-14 14:46:15 +0200

    two more routes added

--- Introtoexpress/express_excercise/app.js
+++ Introtoexpress/express_excercise/app.js
@@ -1,26 +1,34 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get("/", function(req, res){
-    res.send("Hi Welcome to Express second assignment. ")
+app.get("/", function(req, res) {
+  res.send("Hi Welcome to Express second assignment. ");
 });
-app.get("/speak/:animal", function(req, res){
-    var animalName = req.params.animal;
-    var animalRes = "";
-    if(animalName === 'pig'){
-        animalRes = "Oink!!";
-    }
-    else if(animalName == "cow"){
-        animalRes = "Moo";
-    }
-    else if(animalName == "dog"){
-        animalRes = "Woof Woof";
-    }else{
-        animalRes = "Try Again";
-    }
-    res.send(`The ${animalName} says ${animalRes}`);
-})
+app.get("/speak/:animal", function(req, res) {
+  var sounds = {
+    pig: "Oink!",
+    dog: "Woof Woof",
+    cat: "Meow",
+    goldfish: "..."
+  };
+  var animalName = req.params.animal;
+  var animalRes = sounds[animalName];
+  res.send(`The ${animalName} says ' ${animalRes} '`);
+});
 
-app.listen(3000, function(){
-    console.log("Server started at Localhost : 3000");
+app.get("/repeat/:message/:number", function(req, res) {
+  var messageText = req.params.message;
+  var numberOfTimes = Number(req.params.number);
+  var responseResult = "";
+  for (i = 0; i < numberOfTimes; i++) {
+    responseResult = responseResult + " " + messageText;
+  }
+  res.send(responseResult);
+});
+
+app.get("*", function(req, res) {
+  res.send("Page not found. What are you doing with your life?");
+});
+app.listen(3000, function() {
+  console.log("Server started at Localhost : 3000");
 });

8f275d7fea4e837e2b7fb933bb020383753b5ed3
chandu <chandrasr.palle@gmail.com>
2018-06-14 13:57:20 +0200

    second route added

--- Introtoexpress/express_excercise/app.js
+++ Introtoexpress/express_excercise/app.js
@@ -4,6 +4,22 @@ var app = express();
 app.get("/", function(req, res){
     res.send("Hi Welcome to Express second assignment. ")
 });
+app.get("/speak/:animal", function(req, res){
+    var animalName = req.params.animal;
+    var animalRes = "";
+    if(animalName === 'pig'){
+        animalRes = "Oink!!";
+    }
+    else if(animalName == "cow"){
+        animalRes = "Moo";
+    }
+    else if(animalName == "dog"){
+        animalRes = "Woof Woof";
+    }else{
+        animalRes = "Try Again";
+    }
+    res.send(`The ${animalName} says ${animalRes}`);
+})
 
 app.listen(3000, function(){
     console.log("Server started at Localhost : 3000");

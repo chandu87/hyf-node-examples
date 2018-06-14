c7b9d8abc7132cbea3f03a55f75fc624c36f9c25
chandu <chandrasr.palle@gmail.com>
2018-06-14 12:42:20 +0200

    route params

--- Introtoexpress/FirstExpressApp/app.js
+++ Introtoexpress/FirstExpressApp/app.js
@@ -1,9 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.get("*",function(req, res){
-    res.send("Page not found");
-});
+
 app.get("/", function(req, res){
     res.send("Hi there");
 });
@@ -12,7 +10,14 @@ app.get("/bye", function(req, res){
 });
 app.get("/cat", function(req, res){
     res.send("Meow!!!");
-})
+});
+app.get("/r/:subreddit", function(req, res){
+    var subreddit = req.params.subreddit;
+    res.send("It is a " + subreddit.toUpperCase());
+});
+app.get("*",function(req, res){
+    res.send("Page not found");
+});
 
 app.listen(3000, function(){
     console.log("Server started at Port : 3000");

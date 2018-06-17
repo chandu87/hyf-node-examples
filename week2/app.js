4af9bec272ed5d78a0abf7020fd414624b739419
chandu <chandrasr.palle@gmail.com>
2018-06-17 15:12:26 +0200

    new class contact added

--- week2/app.js
+++ week2/app.js
@@ -1,1 +1,9 @@
-console.log("Week2");
+const Contact = require("./Contact.js");
+
+let person1 = new Contact("Richard", "30", "");
+let person2 = new Contact("Mat", "30", "74837383");
+console.log(person1);
+person1.printAll();
+person2.printAll();
+person1.call();
+person2.call();

ae98196a2700a9283492b9010a03ec3e5ffd7a81
chandu <chandrasr.palle@gmail.com>
2018-06-17 16:05:18 +0200

    contactlist added

--- week2/app.js
+++ week2/app.js
@@ -1,9 +1,16 @@
 const Contact = require("./Contact.js");
+const ContactList = require("./ContactList");
 
 let person1 = new Contact("Richard", "30", "");
 let person2 = new Contact("Mat", "30", "74837383");
-console.log(person1);
-person1.printAll();
-person2.printAll();
-person1.call();
-person2.call();
+// console.log(person1);
+// person1.printAll();
+// person2.printAll();
+// person1.call();
+// person2.call(); 
+
+const list = new ContactList("hyf-contacts.json");
+list.add(person1);
+list.add(person2);
+list.save();
+// console.log(list);

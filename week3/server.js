a6eacf2f72ed8db3818cd05cee975912076042da
chandu <chandrasr.palle@gmail.com>
2018-06-24 16:04:07 +0200

    routing added with express

--- week3/server.js
+++ week3/server.js
@@ -1,8 +1,27 @@
-const http = require('http');
+// const http = require('http');
+// const fs = require('fs');
+// const index = fs.readFileSync('src/index.html')
 
-http.createServer(function(req, res){
-    res.write("Hello Server!!");
-    res.end();
-}).listen(3000, function(){
-    console.log("Server started at PORT: 3000");
+// http.createServer(function(req, res){
+//     res.writeHead(200, {'Content-Type':'text/html'});
+//     res.end(index);
+// }).listen(3000, function(){
+//     console.log("Server started at PORT: 3000");
+// });
+
+
+const express = require('express');
+const app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/src/index.html'));
 });
+
+app.get("/hello", function(req, res){
+    res.send("Hello Express");
+});
+app.listen(3000, function(){
+    console.log("Server Started");
+})

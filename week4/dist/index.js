3be7fbf71a0771a19fcbe9ea2d735c38e0aaf3f7
chandu <chandrasr.palle@gmail.com>
2018-07-01 13:22:37 +0200

    basic server created

--- week4/dist/index.js
+++ week4/dist/index.js
@@ -1,4 +1,17 @@
 "use strict";
 
-console.log("it works");
+var _express = require("express");
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const app = (0, _express2.default)();
+
+app.get("/", function (req, res) {
+    res.send("Hello world");
+});
+app.listen(3000, function () {
+    console.log("Server started at : 3000");
+});
 //# sourceMappingURL=index.js.map
--- week4/src/index.js
+++ week4/src/index.js
@@ -1,1 +1,9 @@
-console.log("it works");
+import express from 'express';
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("Hello world");
+});
+app.listen(3000, function(){
+    console.log("Server started at : 3000");
+});

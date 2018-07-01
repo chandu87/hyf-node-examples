31e699f555bdca886069b56af03b7aeb49e49a51
chandu <chandrasr.palle@gmail.com>
2018-07-01 13:47:02 +0200

    new routes added

--- week4/dist/index.js
+++ week4/dist/index.js
@@ -11,6 +11,12 @@ const app = (0, _express2.default)();
 app.get("/", function (req, res) {
     res.send("Hello world");
 });
+app.get("/contact", function (req, res) {
+    res.send("Contact me - chandrasr.palle@gmail.com");
+});
+app.get("/test", function (req, res) {
+    res.send({ name: "chandu" });
+});
 app.listen(3000, function () {
     console.log("Server started at : 3000");
 });
--- week4/src/index.js
+++ week4/src/index.js
@@ -4,6 +4,12 @@ const app = express();
 app.get("/", function(req, res){
     res.send("Hello world");
 });
+app.get("/contact", function(req, res){
+    res.send("Contact me - chandrasr.palle@gmail.com");
+});
+app.get("/test", function(req, res){
+    res.send({name: "chandu"});
+});
 app.listen(3000, function(){
     console.log("Server started at : 3000");
 });
